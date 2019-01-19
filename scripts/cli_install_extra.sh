be18aa191498714ae99b1f9b0eb1fb81e38bf930
Iulian PAUN <pauniulian3144@gmail.com>
2019-01-19 12:06:16 +0200

    remove sudo from install command

--- scripts/cli_install_extra.sh
+++ scripts/cli_install_extra.sh
@@ -2,29 +2,30 @@
 # cli_install_extra.sh - install additional command-line interface (CLI) software for Ubuntu/Debian
 
 # resynchronize the package index files from their sources and install them
-sudo apt-get -y update
-sudo apt-get -y dist-upgrade
-sudo apt-get -y autoremove
+apt-get -y update
+apt-get -y dist-upgrade
+apt-get -y autoremove
 
 # Development tools
-sudo apt-get -y build-essential cmake
+apt-get -y install build-essential cmake
 
 # File archivers
-sudo apt-get -y p7zip p7zip-full unrar-free unzip
+apt-get -y install p7zip p7zip-full unrar-free unzip
 
 # Terminal emulator
-sudo apt-get -y install terminator
+apt-get -y install terminator
 
 # Text editors
-sudo apt-get -y install vim
+apt-get -y install vim
 
 # Python package manager
-sudo apt-get -y install pip
+apt-get -y install pip
+apt-get -y install pip3
 
 # Git
-sudo apt-get -y install git
+apt-get -y install git
 
 # Other useful tools
-sudo apt-get -y install htop pv tree
+apt-get -y install htop pv tree
 
 # TODO: something else may need to be added
