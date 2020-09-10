a7df7db4b29204c58fad251ad38936dc820ab8e8
Iulian PAUN <pauniulian3144@gmail.com>
2020-09-10 23:36:28 +0300

    add script for output timestamping
    Add a script that can be used to print a timestamp for each line of output of a program.
    Usage:
    `command | python timestamp.py`

---
+++ scripts/timestamp.py
@@ -0,0 +1,24 @@
+import sys
+import time
+
+TIMESTAMP_FORMAT = "[{:7.2f} s] "
+
+def main():
+    try:
+        last_timestamp = 0
+        first_timestamp = time.perf_counter()
+        print(TIMESTAMP_FORMAT.format(0) + "begin")
+        for ix, line in enumerate(sys.stdin):
+            # we want the time difference
+            curr_timestamp = time.perf_counter() - first_timestamp
+
+            print(TIMESTAMP_FORMAT.format(curr_timestamp) + f"{line}", end="")
+    except KeyboardInterrupt as e:
+        return
+    finally:
+        curr_timestamp = time.perf_counter() - first_timestamp
+        print(TIMESTAMP_FORMAT.format(curr_timestamp) + "end")
+
+
+if __name__ == '__main__':
+    main()
